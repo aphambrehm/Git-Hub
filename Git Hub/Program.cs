f1279ac0c10f202a1dcf7a8af52ae1234c8e151e
Alex Pham-Brehm <alexphambrehm240@gmail.com>
2021-01-17 22:05:31 -0800

    Add project files.

---
+++ Git Hub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git and Github!");
+        }
+    }
+}
